ea2376cc93bb2b30e7d4614ac06a37b6c04fd1bb
paulelling <paulelling@yahoo.com>
2025-05-04 11:42:38 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <main>
+      <ProjectsSidebar></ProjectsSidebar>
+    </main>
+  );
+}
+
+export default App;
